793c4e3d8790120bfdd7b319dff6448865adddbb
quentin.bolsee <quentin.bolsee@lampiris.be>
2022-01-14 15:02:17 +0100

    Rename element input binding

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -6,7 +6,7 @@ import {Component, Input, OnInit} from '@angular/core';
   styleUrls: ['./server-element.component.css']
 })
 export class ServerElementComponent implements OnInit {
-  @Input() element: {type: string, name: string, content: string} // définir l'objet anonyme sur le tas
+  @Input('serverElement') element: {type: string, name: string, content: string} // définir l'objet anonyme sur le tas
 
   constructor() { }
 
